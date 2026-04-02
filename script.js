b753a66a96a48746474b50f8b18059a24d5fb680
fourmisdesamis-art <fourmisdesamis@gmail.com>
2026-04-02 18:24:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 console.log("Site chargé !");
+
 function toggleMenu() {
   const menu = document.getElementById("navLinks");
   menu.classList.toggle("active");
+}
+
+// INSCRIPTION
+function register() {
+  const username = document.getElementById("registerUsername").value;
+  const password = document.getElementById("registerPassword").value;
+
+  if (!username || !password) {
+    alert("Remplis tous les champs !");
+    return;
+  }
+
+  localStorage.setItem(username, password);
+  alert("Compte créé !");
+}
+
+// CONNEXION
+function login() {
+  const username = document.getElementById("loginUsername").value;
+  const password = document.getElementById("loginPassword").value;
+
+  const storedPassword = localStorage.getItem(username);
+
+  if (storedPassword === password) {
+    alert("Connexion réussie !");
+  } else {
+    alert("Identifiants incorrects !");
+  }
 }
